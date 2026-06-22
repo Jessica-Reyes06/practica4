cfab4c2140bd174656a6e652523cab5252763933
whitetower <dr.mtorreblanca@gmail.com>
2026-06-22 09:51:34 -0600

    Creacion de funciones aritemticas

---
+++ funciones_aritemticas.py
@@ -0,0 +1,19 @@
+
+
+def sumar(a, b):
+    """Suma dos números."""
+    return a + b
+
+def restar(a, b):
+    """Resta dos números."""
+    return a - b
+
+def multiplicar(a, b):  
+    """Multiplica dos números."""
+    return a * b
+
+def dividir(a, b):
+    """Divide dos números."""
+    if b == 0:
+        raise ValueError("No se puede dividir por cero.")
+    return a / b
